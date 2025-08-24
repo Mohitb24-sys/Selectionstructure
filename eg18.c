d0f33fb589c862e6f16794d9c2690a17efe942b5
mohit bharwani <padi.ashok2007@gmail.com>
2025-08-24 20:13:55 +0530

    Create eg18.c

---
+++ eg18.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int l,b,area,perimeter;
+scanf("%d%d%d%d",&l,&b);
+area=l*b;
+printf("area of the rectangle is: %d \n",area);
+perimeter=2*(l+b);
+printf("perimeter of the rectangle is: %d \n",perimeter)

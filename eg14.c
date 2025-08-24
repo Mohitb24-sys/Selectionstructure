9f0b460f23d291be9b737caacb2e7faf8a2d9022
mohit bharwani <padi.ashok2007@gmail.com>
2025-08-24 20:03:16 +0530

    Create eg14.c

---
+++ eg14.c
@@ -0,0 +1,10 @@
+#inlcude<stdio.h>
+int main()
+{
+float fahrehenit,celcius;
+scanf("%f",&celcius);
+fahrehenit=(9/5*celcius)+32;
+printf("temperature in fahreheint: %.2f",farehenit);
+return 0;
+}
+

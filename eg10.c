7ab701faeb03b4c941bf94a43420f4185e4f7c0a
mohit bharwani <padi.ashok2007@gmail.com>
2025-08-24 19:44:35 +0530

    Create eg10.c

---
+++ eg10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float dollars,rupees,pounds;
+printf("ENTER DOLLARS",&dollars);
+scanf("%f",&dollars);
+rupees=dollars*48;
+pounds=ruppes/70;
+printf("%.2f DOLLARS IN POUNDS IS %.2f",dollars,pounds);
+return 0;

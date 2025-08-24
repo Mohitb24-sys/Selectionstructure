9de3e13ec3c053b43177e602fde0a4fcacfdf3eb
mohit bharwani <padi.ashok2007@gmail.com>
2025-08-24 19:38:50 +0530

    Create eg6.c

---
+++ eg6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int hours,minutes;
+scanf("%d",&hours);
+printf("enter the no.of hours: ");
+minutes=hours*60;
+printf("%d hours is equal to %d minutes",minutes,hours);
+return 0;
+}

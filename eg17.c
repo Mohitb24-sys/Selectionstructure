43410a34cf2c48b126bef39f5b26acf567cbcc57
mohit bharwani <padi.ashok2007@gmail.com>
2025-08-24 20:13:04 +0530

    Create eg17.c

---
+++ eg17.c
@@ -0,0 +1,11 @@
+#inclide<stdio.h>
+int main()
+{
+int l,p,a;
+scanf("%f%f%f",l);
+a=l^2;
+printf("area of the square is: %d \n",a);
+p=4*l;
+printf("perimeter of the square is: %d \n",p);
+return 0;
+} 

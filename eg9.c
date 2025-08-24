ebcf3e38761eb5f8d894ae5a46f7ec060b85670f
mohit bharwani <padi.ashok2007@gmail.com>
2025-08-24 19:41:07 +0530

    Create eg9.c

---
+++ eg9.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int dollars,rupees;
+scanf("%d",minutes);
+printf("ENTER RUPEES");
+dollars=rupees/48;
+printf("%d RUPEES IN DOLLARS IS %d",dollars,rupees);
+return 0;
+}

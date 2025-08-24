6d758059f26b61a00f08ab9c4ea566df2f9a48e5
mohit bharwani <padi.ashok2007@gmail.com>
2025-08-24 19:40:16 +0530

    Create eg8.c

---
+++ eg8.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int dollars,rupees;
+scanf("%d",&dollars);
+printf("ENTER DOLLARS");
+rupees=dollars*48;
+printf("%d DOLLARS IN RUPEES IS %d",hours,minutes);
+return 0;
+}
